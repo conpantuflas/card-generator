f465e4fb7d7334fee5d6a2e203b7f156efb867c3
judith carreño <3280kg@gmail.com>
2022-04-30 15:53:50 -0400

    primer commit

---
+++ js.js
@@ -0,0 +1,46 @@
+//----------------------------------------------------------------------{material de trabajo}
+//clases
+
+//botones
+let random = document.getElementById("botonRandom")
+let pinta = document.getElementById("botonPinta")
+let numero = document.getElementById("botonNumero")
+
+//img
+let imagenesCambioUno = document.getElementById("imgUno")
+let imagenesCambioDos = document.getElementById("imgDos")
+// imagenesCambioUno.innerHTML =`
+// <img id="imgConejo" src="/img/conejo.png" alt="conejo"/>
+// <img id="imgGato" src="/img/gato.png" alt="gato"/>
+// <img id="imgOso" src="/img/oso.png" alt="oso"/>
+// <img id="imgPerro" src="/img/perro.png" alt="perro"/>
+// `
+let imgConejo = document.getElementById("imgConejo")
+let imgGato = document.getElementById("imgGato")
+let imgOso = document.getElementById("imgOso")
+let imgPerro = document.getElementById("imgPerro")
+
+//arreglo imagenes
+let imgArreglo = [imgConejo,imgGato,imgOso,imgPerro] 
+
+//cambio numeral de la carta
+let arrNumeros = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","As"]
+
+//----------------------------------------------------------------------------{funcionalidades}
+
+//cambio de pinta
+function cambiarImagenJS(){
+    let numerosCambio = document.getElementsByClassName("numerosCambio").
+  }
+
+//---------------------------------------------------------------
+//cambio de numero
+
+//---------------------------------------------------------------
+//carta aleatoria
+
+//---------------------------------------------------------------
+
+
+
+
